f560364412a56ab5fb23737da30f60f6023f17cb
Dilber Ozer <dilberozer2096@gmail.com>
2023-06-04 19:30:58 +0300

    Update and rename calisan.py to Calisan.py
    Uygun değişkenlerin tanımlanması fonksiyon pass geçildi

---
+++ Calisan.py
@@ -0,0 +1,33 @@
+from Calisan(Insan):
+  def __init__(self,tc_no,ad,soyad,yas,cinsiyet,uyruk,sektor,tecrube,maas):
+    try: 
+      super().__init__(tc_no,ad,soyad,yas,cinsiyet,uyruk)
+      self.__sektor=sektor
+      self.__tecrube=tecrube
+      self.__maas=maas
+    except Exception as e: 
+      print("",str(e))
+  def get_sektor(self):
+        return self.__sektor
+        
+  def get_tecrube(self):
+        return self.__tecrube
+
+  def set_tecrube(self, tecrube):
+        self.__tecrube = tecrube
+
+  def get_maas(self):
+        return self.__maas
+
+  def set_maas(self, maas):
+        self.__maas = maas
+  def zam_hakki(self):
+    pass
+  def __str__(self):
+    try:
+      self.zam_hakki()
+      return f"{super().__str__()}\nTecrübe: {self.__tecrube}\nYeni Maaş: {self.__maas}"
+    except Exception as e:
+      print("Hata: ",str(e))
+     
+    
