04360a1a13ef6939983ef8422a7f797f15d6da5c
Dilber Ozer <dilberozer2096@gmail.com>
2023-06-04 20:30:38 +0300

    Update Issiz.py
    Insan sınınfından inheritance yapılması
    gerekli değişkenlerin tanımlanması
    ve get-set metodlarının kullanılması
    statu_bul fonksiyonunun tanımlanması
    try - except tanımlanması

--- Issiz.py
+++ Issiz.py
@@ -1,1 +1,31 @@
+from Insan import Insan
+class Issiz(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, statuler):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__statuler=statuler
+         
+    def get_statuler(self):
+        return self.__statuler
 
+    def set_statuler(self):
+        self.__statuler=statuler
+
+    def statu_bul(self):
+        try:
+            max_tecrube = max(self.__statuler.values())
+            statu = ''
+            for key, value in self.__statuler.items():
+                if value == max_tecrube:
+                    statu = key
+                    break
+
+            return statu
+        except ValueError:
+            return None
+
+    def __str__(self):
+        statu = self.statu_bul()
+        if statu is not None:
+            return f"Ad: {self.ad}\nSoyad: {self.soyad}\nEn uygun statü: {statu}"
+        else:
+            return f"Ad: {self.ad}\nSoyad: {self.soyad}\nUygun statü bulunamadı."
